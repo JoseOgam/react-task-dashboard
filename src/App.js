df7bbfc6171caa4aad9f554d2b23eb1f8e655a76
JoseOgam <jotienoochieng@gmail.com>
2021-03-15 12:10:04 -0400

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Switch } from 'react-router'
+import Home from './components/home/Home';
 import Login from './components/login'
-// import Register from './components/register'
+import Register from './components/register'
 import './styles/App.scss'
 function App() {
   return (
     <div className="App">
-      <Login /> 
-      {/* <Register /> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={ Login } />
+        <Route path="/register" component={Register} />
+     </Switch>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello Home</h1>
+        </div>
+    )
+}
+export default Home
